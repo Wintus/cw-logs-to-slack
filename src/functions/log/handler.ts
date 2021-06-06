4f781ee1f6c5631a47e8a18dfdcba07458924164
wintus <6386129+wintus@users.noreply.github.com>
2021-06-07 02:56:02 +0900

    Implement decoder

--- src/functions/log/handler.ts
+++ src/functions/log/handler.ts
@@ -1,16 +1,24 @@
 import "source-map-support/register";
-
-import { middyfy } from "@libs/lambda";
 import type { Handler } from "aws-lambda";
 import type { FromSchema } from "json-schema-to-ts";
-
 import { schema } from "./schema";
+import { promisify } from "util";
+import { unzip } from "zlib";
+import { middyfy } from "@libs/lambda";
 
 type Schema = FromSchema<typeof schema>;
 
-const logHandler: Handler<Schema> = async (event) => {
-  const data = event.awslogs.data;
-  console.debug(data);
+const unzipAsync = promisify(unzip);
+
+const decode = async (data: string) => {
+  const buffer = Buffer.from(data, "base64");
+  return unzipAsync(buffer);
+};
+
+const logHandler: Handler<Schema> = async ({ awslogs: { data } }) => {
+  const decoded = await decode(data);
+  const log = decoded.toString("ascii");
+  console.debug(log);
   return;
 };
 
