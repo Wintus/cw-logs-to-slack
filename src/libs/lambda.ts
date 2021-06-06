f464efa9fa67489fadd70ee9e4069be81afa57d9
wintus <6386129+wintus@users.noreply.github.com>
2021-06-07 02:15:48 +0900

    Fix by strict type check

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,9 +1,10 @@
 import type { AWS } from "@serverless/typescript";
-export type FunctionConfig = AWS["functions"][string];
+import type { Handler } from "aws-lambda";
+
+export type FunctionConfig = NonNullable<AWS["functions"]>[string];
 
 import middy from "@middy/core";
 import middyJsonBodyParser from "@middy/http-json-body-parser";
 
-export const middyfy = (handler) => {
-  return middy(handler).use(middyJsonBodyParser());
-};
+export const middyfy = (handler: Handler) =>
+  middy(handler).use(middyJsonBodyParser());
